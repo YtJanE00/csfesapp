081a3c51e4c5d78ff7ba42e67b17ccaadf24a73e
mycodesgit <lowexian@gmail.com>
2025-03-27 20:45:58 +0800

    Update

--- resources/views/layout/master_layout.blade.php
+++ resources/views/layout/master_layout.blade.php
@@ -373,7 +373,7 @@
                     },
                     success: function(response) {
                         $('#editQuestionModal').modal('hide');
-                        location.reload(); // Refresh table to show updates
+                        location.reload();
                     },
                     error: function(xhr) {
                         alert("Something went wrong!");
